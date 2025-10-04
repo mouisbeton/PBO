d2f17fd1a88bcaeca08fa7cfd3c6cbd003f42ff1
Uwais Achmad <uwaisachmad05@gmail.com>
2025-10-04 12:54:17 +0700

    Pertemuan 6

---
+++ Pertemuan-6/book.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+class Book {
+    private String title;
+    private String author;
+    private int year;
+    
+    public Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+    
+    @Override
+    public String toString() {
+        return "Judul: " + title + " | Penulis: " + author + " | Tahun: " + year;
+    }
+}
